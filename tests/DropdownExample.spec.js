f77c4097e05c5e08b414cd8175c80d88b3fcd0c6
Anesh Kumar <aneshlohana0@gmail.com>
2025-10-15 19:53:01 +0500

    dropdownExample file added

---
+++ tests/DropdownExample.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').first().click();
+  await page.getByRole('option', { name: 'ESS' }).click();
+  await page.locator('div:nth-child(4) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click();
+  await page.getByRole('option', { name: 'Disabled' }).click();
+});
