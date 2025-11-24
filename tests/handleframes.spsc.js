1a334cd30090cc8bcb9b53e2bd90e72427281bdc
Anesh Kumar <aneshlohana0@gmail.com>
2025-11-24 14:16:37 +0500

    handle Frame .js added

---
+++ tests/handleframes.spsc.js
@@ -0,0 +1,16 @@
+const {test, expect, devices} = require('@playwright/test');
+
+
+test("Handle Iframes", async ({page})=>{
+    await page.goto("https://docs.oracle.com/javase/8/docs/api/");
+
+    const iframe = page.frameLocator("//frame[@name='packageListFrame']");
+    await iframe.locator("//a[text()='java.applet']").click();
+
+    await page.pause();
+    
+   
+
+
+
+})
